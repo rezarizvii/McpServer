dd44b3f089d06d54a25d9faf31038f4eeae28a28
reza-rizvi <reza.rizvi@artesian.io>
2025-09-22 14:46:22 +0530

    Added the ai-plugin.json file to describe the tools available in the server.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Net.Http;
 using System.Web;                // for HttpUtility
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
@@ -11,6 +12,13 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, ".well-known")),
+    RequestPath = "/.well-known"
+});
 
 app.MapEstimatesEndpoints();
 app.MapDocketsEndpoints();
